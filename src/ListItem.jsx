af3b1eee659d0c2ed85d5793a2b6a8dc0ac4d57e
andrewbantly <andrewbantly@gmail.com>
2023-04-17 18:02:47 -0700

    deliverable complete

---
+++ src/ListItem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import 
+'./App.css';
+
+export default class ListItem extends Component {
+    render() {
+        return(
+            <li>{this.props.task}</li>
+        )
+    }
+}
